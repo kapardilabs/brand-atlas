4e7d65b641e3502fb5fc9c778e53f7d18e604339
Ganesh Sharma <sosukegani.tmk@gmail.com>
2020-06-20 09:46:17 +0530

    CSVs :)

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 
 
 # Register your models here.
-class ConfigAdmin(admin.ModelAdmin):
-    readonly_fields = ("id",)
+class ConfigAdmin(ImportExportModelAdmin):
+    resource_class = CrawlConfig
+
+
+class BrandAdmin(ImportExportModelAdmin):
+    resource_class = Brand
+
+
+class CountryAdmin(ImportExportModelAdmin):
+    resource_class = Country
 
 
 admin.site.register(CrawlConfig, ConfigAdmin)
-admin.site.register(Country)
-admin.site.register(Brand)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Brand, BrandAdmin)
